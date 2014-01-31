a84a871a44aeb6c5331d31ede82e89c67df88b0c
KenjiBaheux <kenji@baheux.net>
2014-01-31 11:26:14 +0900

    Extension to remap capslock on external keyboard
    Partial implementation; remapped to Ctrl. Needs more work.

---
+++ capslockremap/background.js
@@ -0,0 +1,95 @@
+/*
+Copyright 2014 Google Inc. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+var contextID = -1;
+var remapTo = "a";
+var lastRemappedKeyEvent = "";
+var ctrlKey = false;
+
+chrome.input.ime.onFocus.addListener(function(context) {
+  contextID = context.contextID;
+});
+
+chrome.input.ime.onBlur.addListener(function(context) {
+  contextID = -1;
+});
+
+var lut = {"A":"a"}; // TODO complete the whole table of Shifted key => unshifted key
+
+function isCapsLock(keyData) {
+   return (keyData.code == "CapsLock");
+};
+
+function isRemappedEvent(keyData) {  
+ // hack, should check for a sender ID (to be added to KeyData)
+ return lastRemappedKeyEvent != undefined &&
+        (lastRemappedKeyEvent.key == keyData.key &&
+         lastRemappedKeyEvent.code == keyData.code &&
+         lastRemappedKeyEvent.type == keyData.type
+        ); // requestID would be different so we are not checking for it  
+}
+
+function unshift(keyData) {
+  if (lut[keyData.key]) { 
+    keyData.key = lut[keyData.key];
+  }
+}
+
+
+
+chrome.input.ime.onKeyEvent.addListener(
+    function(engineID, keyData) {
+      var handled = false;
+      
+      if (isRemappedEvent(keyData)) {
+        console.log(keyData); // TODO eventually remove
+        return false;
+      }
+      
+      
+      if (isCapsLock(keyData)) {
+          if (chrome.input.ime.sendKeyEvents != undefined) {
+            keyData.code = "ControlLeft";
+            keyData.key = "Ctrl";
+            keyData.ctrlKey = (keyData.type == "keydown");
+            ctrlKey = keyData.ctrlKey;
+            keyData.capsLock = false;
+            chrome.input.ime.sendKeyEvents({"contextID": contextID, "keyData": [keyData]});
+            lastRemappedKeyEvent = keyData;                                                                 
+          } else {
+	      // TODO remove (can't be done without sendKeyEvents)
+            if (keyData.type == "keydown") {
+            chrome.input.ime.commitText({"contextID": contextID,
+                                   "text": remapTo});
+            } else {
+               console.log("keyup capslock");
+            }
+          }          
+        handled = true;
+      } else if (ctrlKey) {
+        keyData.ctrlKey = ctrlKey;
+        chrome.input.ime.sendKeyEvents({"contextID": contextID, "keyData": [keyData]});
+        lastRemappedKeyEvent = keyData;
+       handled = true;
+      } else if (keyData.capsLock) {
+	// TODO need to unshift keyData
+        keyData.capsLock = false;
+        chrome.input.ime.sendKeyEvents({"contextID": contextID, "keyData": [keyData]});
+        lastRemappedKeyEvent = keyData;
+        handled = true;
+      }
+      
+      return handled;
+});
