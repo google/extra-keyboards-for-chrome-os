6dd1a1d3134fdd9355571e05c5ab2ab0a109e42b
Dafydd Roberts <nubotix@gmail.com>
2020-08-10 15:14:15 +0100

    arbrofi a

--- uk2cy/background.js
+++ uk2cy/background.js
@@ -1,90 +1,53 @@
-/*
-Copyright 2014 Google Inc. All rights reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-var AltGr = { PLAIN: "plain", ALTERNATE: "alternate" };
-var Shift = { PLAIN: "plain", SHIFTED: "shifted" };
-
-var contextID = -1;
-var altGrState = AltGr.PLAIN;
-var shiftState = Shift.PLAIN;
-var lastRemappedKeyEvent = undefined;
-
-var lut = {
-"IntlBackslash": { "plain": {"plain": "Shift", "shifted": "Shift"}, "alternate": {"plain": "", "shifted":""}, "code": "ShiftLeft", "shiftKey": true},
-"Backslash": { "plain": {"plain": "Enter", "shifted": "Enter"}, "alternate": {"plain": "", "shifted":""}, "code": "Enter", "shiftKey": false},
-"KeyQ": { "plain": {"plain": "6", "shifted": "%"}, "alternate": {"plain": "", "shifted":""}, "code": "Digit5", "shiftKey": false},
-};
-    
+var context_id = -1;
 
 chrome.input.ime.onFocus.addListener(function(context) {
-  contextID = context.contextID;
+  context_id = context.contextID;
 });
 
-function updateAltGrState(keyData) {
-  altGrState = (keyData.code == "AltRight") ? ((keyData.type == "keydown") ? AltGr.ALTERNATE : AltGr.PLAIN)
-                                              : altGrState;
-}
-
-function updateShiftState(keyData) {
-  shiftState = ((keyData.shiftKey && !(keyData.capsLock)) || (!(keyData.shiftKey) && keyData.capsLock)) ?
-                 Shift.SHIFTED : Shift.PLAIN;
-}
-
-function isPureModifier(keyData) {
-  return (keyData.key == "Shift") || (keyData.key == "Ctrl") || (keyData.key == "Alt");
-}
-
-function isRemappedEvent(keyData) {
-  // hack, should check for a sender ID (to be added to KeyData)
-  return lastRemappedKeyEvent != undefined &&
-         (lastRemappedKeyEvent.key == keyData.key &&
-          lastRemappedKeyEvent.code == keyData.code &&
-          lastRemappedKeyEvent.type == keyData.type
-         ); // requestID would be different so we are not checking for it
-}
-
+var shifted = false;
 
 chrome.input.ime.onKeyEvent.addListener(
-    function(engineID, keyData) {
-      var handled = false;
-      
-      if (isRemappedEvent(keyData)) {
-        lastRemappedKeyEvent = undefined;
-        return handled;
+  function(engineID, keyData) {
+    var handled = false;
+    
+    if (keyData.type == "keydown") {
+      if (keyData.code == "AltRight") {
+        if (keyData.code == "KeyA") {
+          keyData.code = "\u00e2";
+        }
+        //keyData.code = "Enter";
+        //chrome.input.ime.sendKeyEvents({"contextID": context_id, "keyData": [keyData]});
+        handled = true;
       }
-
-      updateAltGrState(keyData);
-      updateShiftState(keyData);
-                
-      if (lut[keyData.code]) {
-          var remappedKeyData = keyData;
-          remappedKeyData.key = lut[keyData.code][altGrState][shiftState];
-          remappedKeyData.code = lut[keyData.code].code;
-          // remappedKeyData.shiftKey = lut[keyData.code].shiftKey;
+        // if (keyData.code == "Backslash") {
+        // keyData.code = "Enter";
+        // chrome.input.ime.sendKeyEvents({"contextID": context_id, "keyData": [keyData]});
+        // handled = true;
+        //}
+      else if (keyData.key == "Shift") {
+        // keyData.key = "Shift";
+        // keyData.code = "ShiftLeft";
+        // keyData.shiftKey = true;
+        // chrome.input.ime.sendKeyEvents({"contextID": context_id, "keyData": [keyData]});
         
-        if (chrome.input.ime.sendKeyEvents !== undefined) {
-          chrome.input.ime.sendKeyEvents({"contextID": contextID, "keyData": [remappedKeyData]});
-          handled = true;
-          lastRemappedKeyEvent = remappedKeyEvent;
-        }
-        else
-        if (keyData.type == "keydown" && !isPureModifier(keyData)) {
-           chrome.input.ime.commitText({"contextID": contextID, "text": remappedKeyData.key});
-           handled = true;
-        }
+        shifted = true;
+        handled = true;
+
+      } else  if (shifted) {
+        keyData.shiftKey = true;
+        chrome.input.ime.commitText({"contextID": context_id, "text": keyData.key.toUpperCase()});
+        handled = true;
       }
-      
-      return handled;
+    } else if (keyData.type == "keyup") {
+      if (keyData.code == "AltRight") {
+        // keyData.key = "Shift";
+        // keyData.code = "ShiftLeft";
+        // chrome.input.ime.sendKeyEvents({"contextID": context_id, "keyData": [keyData]});
+        
+        shifted = false;
+        handled = true;
+      }
+    }
+    
+    return handled;
 });
