46eb71d41b3aa7a6acdf5125cb9244c249e440f2
Dafydd Roberts <nubotix@gmail.com>
2020-08-10 12:02:55 +0100

    arbrofi uk2cy

---
+++ uk2cy/background(cadarn).js
@@ -0,0 +1,44 @@
+var context_id = -1;
+
+chrome.input.ime.onFocus.addListener(function(context) {
+  context_id = context.contextID;
+});
+
+var shifted = false;
+
+chrome.input.ime.onKeyEvent.addListener(
+  function(engineID, keyData) {
+    var handled = false;
+    
+    if (keyData.type == "keydown") {
+      if (keyData.code == "Backslash") {
+        keyData.code = "Enter";
+        chrome.input.ime.sendKeyEvents({"contextID": context_id, "keyData": [keyData]});
+        handled = true;
+      }
+      else if (keyData.code == "IntlBackslash") {
+        // keyData.key = "Shift";
+        // keyData.code = "ShiftLeft";
+        // keyData.shiftKey = true;
+        // chrome.input.ime.sendKeyEvents({"contextID": context_id, "keyData": [keyData]});
+        
+        shifted = true;
+        handled = true;
+      } else  if (shifted) {
+        keyData.shiftKey = true;
+        chrome.input.ime.commitText({"contextID": context_id, "text": keyData.key.toUpperCase()});
+        handled = true;
+      }
+    } else if (keyData.type == "keyup") {
+      if (keyData.code == "IntlBackslash") {
+        // keyData.key = "Shift";
+        // keyData.code = "ShiftLeft";
+        // chrome.input.ime.sendKeyEvents({"contextID": context_id, "keyData": [keyData]});
+        
+        shifted = false;
+        handled = true;
+      }
+    }
+    
+    return handled;
+});
