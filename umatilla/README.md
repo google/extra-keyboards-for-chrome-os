2d455c0e5fd7fade575f3cf0153e717701395f9d
Brian Derr <bderrly@gmail.com>
2020-09-18 11:50:27 -0700

    Initial commit of Umatilla layout.

---
+++ umatilla/README.md
@@ -0,0 +1,31 @@
+# Umatilla Layout
+
+This extension adds a Umatilla-Sahaptian keyboard layout to Chrome OS.
+
+## How To Install
+
+1. Go to `chrome://extensions`.
+   1. Toggle "Developer mode" (in the upper right corner) on.
+   2. Click "Load unpacked" on the top left.
+   3. Navigate to and select the folder containing the manifest for Umatilla
+      input and push "Open".
+
+2. Go to `chrome://settings/languages`.
+   1. Select "Language" and ensure "English (United States)" is listed or add
+      it if it is missing.
+   2. Select "Input method" followed by "Manage input methods".
+   3. Select the box labeled "Umatilla".
+
+
+## How To Use
+
+A new icon should appear in the status bar of Chrome OS indicating which
+keyboard layout is active. The default for "US Keyboard" will display as "US".
+Type `Shift+Ctrl+Space` to cycle through the available language and layout
+options. The Umatilla layout should be shown as "EN". Note that other layouts
+using the English language with a custom layout will look that way as well.
+
+Click the icon for a pop-up which will show all available layouts and which
+is currently active. You may change layouts this way as well.
+
+Layout by Dave Sienko (https://www.tulaliplushootseed.com)
---
+++ umatilla/background.js
@@ -0,0 +1,68 @@
+/*
+Copyright 2020 The Extra Keyboards for Chrome OS Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+var contextID = 0;
+
+// Each key in this lookup table contains an array with two values: the
+// unshifted and shifted glyph to output.
+var lut = {
+  "Backquote": [ "š", "~" ],
+  "Minus": [ "č̓", "c̓" ],
+  "Equal": [ "č", "x̣ʷ" ],    
+  "BracketLeft": [ "ł", "q̓ʷ" ],
+  "BracketRight": [ "ɨ", "k̓ʷ" ],
+  "Backslash": [ "ƛ", "x̣" ],
+  "Semicolon": [ "ʷ", ""̌"],
+  "Quote": [ ""̓", "ʔ" ],
+  "Period": [ ".", "̣̣" ],
+  "Slash": [ "́", "̀" ],
+};
+
+chrome.input.ime.onFocus.addListener(
+    function(context) {
+      contextID = context.contextID;
+    }
+);
+
+chrome.input.ime.onBlur.addListener(() => {
+  contextID = 0;
+})
+
+chrome.input.ime.onKeyEvent.addListener(
+    function(engineID, keyData) {
+      var handled = false;
+      
+      if (keyData.type == "keydown") {
+        if (lut[keyData.code]) {
+          let shifted = keyData.capsLock ^ keyData.shiftKey;
+          let emit = lut[keyData.code][shifted];
+
+          if (emit != null && contextID != 0) {
+            chrome.input.ime.commitText({
+              "contextID": contextID,
+              "text": emit,
+            }, () => {
+              if (chrome.runtime.lastError) {
+                console.error('Error committing text:', chrome.runtime.lastError);
+                return;
+              }
+            });
+          }
+          handled = true;
+        }
+      }
+      return handled;
+});
